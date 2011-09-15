a07f28a17182da09f39b715f3f18b48b03d802a6
Raphael Kallensee <raphael@kallensee.name>
2011-09-15 21:34:42 +0200

    Added Fancybox for Flickr images, more headlines and CSS style.

--- contents/resources/script.js
+++ contents/resources/script.js
@@ -13,7 +13,16 @@ $(document).ready(function() {
 	$("#flickrPics").jflickrfeed({
 		limit: 15,
 		qstrings: { tags: "wmmrn" },
-		itemTemplate: "<a href='{{image_b}}'><img src='{{image_s}}' alt='{{title}}' title='{{title}}'/></a>"
+		itemTemplate: "<a href='{{image_b}}' rel='flickr-feed' title='{{title}}'><img src='{{image_s}}' alt='{{title}}' title='{{title}}'/></a>"
+	}, function(data) {
+	    jQuery("#flickrPics a").fancybox({
+		    'transitionIn'	:	'elastic',
+		    'transitionOut'	:	'elastic',
+		    'speedIn'		:	600,
+		    'speedOut'		:	200,
+		    //'overlayShow'	:	false,
+		    'hideOnContentClick': true
+	    });
 	});
-
+	
 });
