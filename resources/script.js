90c383753441612f2d32d1823a4b9ec6c438aa63
Raphael <raphael@kallensee.name>
2012-05-30 23:19:36 +0200

    Synced from wmmrn.de

--- resources/script.js
+++ resources/script.js
@@ -1,28 +1,33 @@
 
 $(document).ready(function() {
 
-	$(".tweet").tweet({
-		//username: "wmmrn",
-		query: "wmmrn",
-		avatar_size: 48,
-		count: 7,
-		loading_text: "loading tweets..."
-		//, template: "{text} <br/>» {retweet_action}"
-	});
+	if ($().tweet) {
+		$('.tweet').tweet({
+			username: 'wmmrn',
+			//query: '#wmmrn',
+			avatar_size: 48,
+			count: 7,
+			loading_text: 'loading tweets...'
+			//, template: '{text} <br/>» {retweet_action}'
+		});
+	}
 
-	$("#flickrPics").jflickrfeed({
-		limit: 15,
-		qstrings: { tags: "wmmrn" },
-		itemTemplate: "<a href='{{image_b}}' rel='flickr-feed' title='{{title}}'><img src='{{image_s}}' alt='{{title}}' title='{{title}}'/></a>"
-	}, function(data) {
-	    jQuery("#flickrPics a").fancybox({
-		    'transitionIn'	:	'elastic',
-		    'transitionOut'	:	'elastic',
-		    'speedIn'		:	600,
-		    'speedOut'		:	200,
-		    //'overlayShow'	:	false,
-		    'hideOnContentClick': true
-	    });
-	});
-	
+	if ($().jflickrfeed) {
+		$('#flickrPics').jflickrfeed({
+			limit: 15,
+			qstrings: { tags: 'wmmrn' },
+			itemTemplate: '<a href="{{image_b}}" rel="flickr-feed" title="{{title}}">'+
+				'<img src="{{image_s}}" alt="{{title}}" title="{{title}}"/></a>'
+		}, function(data) {
+			jQuery('#flickrPics a').fancybox({
+				'transitionIn'	:	'elastic',
+				'transitionOut'	:	'elastic',
+				'speedIn'		:	600,
+				'speedOut'		:	200,
+				//'overlayShow'	:	false,
+				'hideOnContentClick': true
+			});
+		});
+	}
+
 });
